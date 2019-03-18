bfa7f4447c4248f854fb73ff573f8ad1785b2fc8
Michael Christian <mchri18@lsu.edu>
2019-03-18 12:49:38 -0500

    added picking up and dropping weapons

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -5,62 +5,75 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController2D controller;
-    public GameObject Weapon;
-    public Animator animator;
-    public float runSpeed = 40f;
-    float horizontalMove = 0f;
-    bool jump = false;
-    bool crouch = false;
-    bool nearWeapon = false;
-    bool hasWeapon = false;
+  public CharacterController2D controller;
+  public GameObject Player;
+  public Animator animator;
+  public float runSpeed = 40f;
+  float horizontalMove = 0f;
+  bool jump = false;
+  bool crouch = false;
 
-    void Update()
+
+  void Update()
+  {
+    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+    if (Input.GetButtonDown("Jump"))
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-            animator.SetBool("IsJumping", true);
-        }
-
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        } 
-        else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        }
-
-        // if weapon = false && collide with weapon set weapon to true, set weapon component position
-        if (hasWeapon == false) {
-            
-        }
-
-        // if press e is pressed drop weapon and despawn
-        if (KeyCode.E && hasWeapon == true){
-            hasWeapon = false;
-            Destroy(Weapon);
-        }
+      jump = true;
+      animator.SetBool("IsJumping", true);
     }
 
-    void FixedUpdate()
+    if (Input.GetButtonDown("Crouch"))
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
+      crouch = true;
+    }
+    else if (Input.GetButtonUp("Crouch"))
+    {
+      crouch = false;
     }
 
-    public void OnLanding() 
-    {
-        animator.SetBool("IsJumping", false);
-    }
+  }
 
-    public void OnCrouching(bool isCrouching)
+  void FixedUpdate()
+  {
+    controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+    jump = false;
+  }
+
+  public void OnLanding()
+  {
+    animator.SetBool("IsJumping", false);
+  }
+
+  public void OnCrouching(bool isCrouching)
+  {
+    animator.SetBool("IsCrouching", isCrouching);
+  }
+
+  void OnTriggerEnter2D(Collider2D col)
+  {
+    //when a player collides make weapon a child of the player if they dont already have a weapon
+    if (Player.transform.childCount == 2)
     {
-        animator.SetBool("IsCrouching", isCrouching);
+      col.transform.parent = Player.transform;
+      if(col.name == "SubMachineGun"){
+          col.transform.localPosition = new Vector3(0.195f, -0.161f, 0);
+      }
+      else if(col.name == "RocketLauncher"){
+          col.transform.localPosition = new Vector3(-0.003f, -0.094f, 0);
+      }
+      else if (col.name == "Pistol"){
+        col.transform.localPosition = new Vector3(0.368f, -0.14f, 0);
+      }
+      else if (col.name == "Grenade"){
+        col.transform.localPosition = new Vector3(-0.2557f, -0.2155f, 0);
+      }
+      else if (col.name == "Knife"){
+        col.transform.localPosition = new Vector3(0.52f, -0.25f, 0);
+      }
     }
+  }
 }
