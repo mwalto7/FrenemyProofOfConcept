23bb419b2b13a6eea34c1e73a173312ec17a6821
Michael Christian <mchri18@lsu.edu>
2019-03-18 19:20:18 -0500

    level 1 started

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -13,24 +13,27 @@ public class PlayerMovement : MonoBehaviour
   bool jump = false;
   bool crouch = false;
   bool facingRight;
+  public int playerNum;
 
   void Update()
   {
-    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    horizontalMove = Input.GetAxisRaw("Horizontal p"+playerNum) * runSpeed;
 
     animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-    if (Input.GetButtonDown("Jump"))
+    if (Input.GetButtonDown("Jump p"+playerNum))
     {
       jump = true;
+      Debug.Log("Jumping");
       animator.SetBool("IsJumping", true);
     }
 
-    if (Input.GetButtonDown("Crouch"))
+    if (Input.GetButtonDown("Crouch p"+playerNum))
     {
+      Debug.Log("crounching");
       crouch = true;
     }
-    else if (Input.GetButtonUp("Crouch"))
+    else if (Input.GetButtonUp("Crouch p"+playerNum))
     {
       crouch = false;
     }
@@ -51,6 +54,7 @@ public class PlayerMovement : MonoBehaviour
 
   public void OnCrouching(bool isCrouching)
   {
+    Debug.Log(isCrouching);
     animator.SetBool("isCrouching", isCrouching);
   }
 
@@ -59,73 +63,79 @@ public class PlayerMovement : MonoBehaviour
     //when a player collides make weapon a child of the player if they dont already have a weapon
     if (Player.transform.childCount == 2)
     {
-      if (Input.GetButtonDown("pickup weapon"))
+      if (Input.GetButtonDown("pickup weapon p"+playerNum))
       {
-        col.transform.parent = Player.transform;
-        if (col.name == "SubMachineGun")
+        if (col.tag == "Weapon")
         {
-          col.transform.localPosition = new Vector3(0.195f, -0.161f, 0);
-        }
-        else if (col.name == "RocketLauncher")
-        {
-          col.transform.localPosition = new Vector3(-0.003f, -0.094f, 0);
-        }
-        else if (col.name == "Pistol")
-        {
-          col.transform.localPosition = new Vector3(0.368f, -0.14f, 0);
-        }
-        else if (col.name == "Grenade")
-        {
-          col.transform.localPosition = new Vector3(-0.2557f, -0.2155f, 0);
-        }
-        else if (col.name == "Knife")
-        {
-          col.transform.localPosition = new Vector3(0.52f, -0.25f, 0);
-        }
-        if (Player.transform.GetChild(2).transform.localScale.x < 0 && !facingRight)
-        {
-          Debug.Log("flipping weapon");
-          Vector3 scale = Player.transform.GetChild(2).transform.localScale;
-          scale.x *= -1;
-          Player.transform.GetChild(2).transform.localScale = scale;
+          col.transform.parent = Player.transform;
+          if (col.name == "SubMachineGun")
+          {
+            col.transform.localPosition = new Vector3(0.195f, -0.161f, 0);
+          }
+          else if (col.name == "RocketLauncher")
+          {
+            col.transform.localPosition = new Vector3(-0.003f, -0.094f, 0);
+          }
+          else if (col.name == "Pistol")
+          {
+            col.transform.localPosition = new Vector3(0.368f, -0.14f, 0);
+          }
+          else if (col.name == "Grenade")
+          {
+            col.transform.localPosition = new Vector3(-0.2557f, -0.2155f, 0);
+          }
+          else if (col.name == "Knife")
+          {
+            col.transform.localPosition = new Vector3(0.52f, -0.25f, 0);
+          }
+          if (Player.transform.GetChild(2).transform.localScale.x < 0 && !facingRight)
+          {
+            Debug.Log("flipping weapon");
+            Vector3 scale = Player.transform.GetChild(2).transform.localScale;
+            scale.x *= -1;
+            Player.transform.GetChild(2).transform.localScale = scale;
+          }
         }
       }
     }
     else if (Player.transform.childCount == 3)
     {
-      if (Input.GetButtonDown("pickup weapon"))
+      if (Input.GetButtonDown("pickup weapon p"+playerNum))
       {
-        col.transform.parent = Player.transform;
-        if (Player.transform.childCount == 4)
+        if (col.tag == "Weapon")
         {
-          Player.transform.GetChild(2).gameObject.transform.parent = null;
-        }
-        if (col.name == "SubMachineGun")
-        {
-          col.transform.localPosition = new Vector3(0.195f, -0.161f, 0);
-        }
-        else if (col.name == "RocketLauncher")
-        {
-          col.transform.localPosition = new Vector3(-0.003f, -0.094f, 0);
-        }
-        else if (col.name == "Pistol")
-        {
-          col.transform.localPosition = new Vector3(0.368f, -0.14f, 0);
-        }
-        else if (col.name == "Grenade")
-        {
-          col.transform.localPosition = new Vector3(-0.2557f, -0.2155f, 0);
-        }
-        else if (col.name == "Knife")
-        {
-          col.transform.localPosition = new Vector3(0.52f, -0.25f, 0);
-        }
-        if (Player.transform.GetChild(2).transform.localScale.x < 0 && !facingRight)
-        {
-          Debug.Log("flipping weapon");
-          Vector3 scale = Player.transform.GetChild(2).transform.localScale;
-          scale.x *= -1;
-          Player.transform.GetChild(2).transform.localScale = scale;
+          col.transform.parent = Player.transform;
+          if (Player.transform.childCount == 4)
+          {
+            Player.transform.GetChild(2).gameObject.transform.parent = null;
+          }
+          if (col.name == "SubMachineGun")
+          {
+            col.transform.localPosition = new Vector3(0.195f, -0.161f, 0);
+          }
+          else if (col.name == "RocketLauncher")
+          {
+            col.transform.localPosition = new Vector3(-0.003f, -0.094f, 0);
+          }
+          else if (col.name == "Pistol")
+          {
+            col.transform.localPosition = new Vector3(0.368f, -0.14f, 0);
+          }
+          else if (col.name == "Grenade")
+          {
+            col.transform.localPosition = new Vector3(-0.2557f, -0.2155f, 0);
+          }
+          else if (col.name == "Knife")
+          {
+            col.transform.localPosition = new Vector3(0.52f, -0.25f, 0);
+          }
+          if (Player.transform.GetChild(2).transform.localScale.x < 0 && !facingRight)
+          {
+            Debug.Log("flipping weapon");
+            Vector3 scale = Player.transform.GetChild(2).transform.localScale;
+            scale.x *= -1;
+            Player.transform.GetChild(2).transform.localScale = scale;
+          }
         }
       }
     }
